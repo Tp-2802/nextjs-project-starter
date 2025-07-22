9f6b6ea2219434afd84104c8d032bfc472deaa12
Tanmay Patel <141579810+Tp-2802@users.noreply.github.com>
2025-07-22 23:04:45 +0530

    Add .next/static/chunks/src_app_dashboard_attendance_page_tsx_bff1e8e7._.js 1753205685047

---
+++ .next/static/chunks/src_app_dashboard_attendance_page_tsx_bff1e8e7._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_0fbd1623._.js",
+  "static/chunks/node_modules_e2cf94fd._.js"
+],
+    source: "dynamic"
+});
